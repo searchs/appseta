dc85c70bf95b56b272dc58d5aeaf5028ca5c794a
Ola Ajibode <ola.ajibode@gmail.com>
2017-01-05 09:59:45 +0000

    add ssl fix for mac osx machines

--- macosconfig.sh
+++ macosconfig.sh
@@ -1,2 +1,5 @@
 #On the terminal, run command to make Finder show hidden files
 defaults write com.apple.finder AppleShowAllFiles TRUE
+
+#Fix SSL Issue on Mac OSX - Mavericks upward
+export LDFLAGS=-L/usr/local/opt/openssl/lib && export CPPFLAGS=-I/usr/local/opt/openssl/include
